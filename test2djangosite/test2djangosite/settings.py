789fbf0992491b37206fa5840c74aa8e94fa3e32
xxz <xxz911@rambler.ru>
2022-10-02 23:49:37 +0300

    Added model and used migrations

--- test2djangosite/test2djangosite/settings.py
+++ test2djangosite/test2djangosite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ugv0&20h_z+tf=9r=93g+q(w+56jfh71w1fl35z8&43jty7#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- test2djangosite/test2djangosite/urls.py
+++ test2djangosite/test2djangosite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 
 from cgitb import handler
+from test2djangosite import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from women.views import *
@@ -24,5 +26,8 @@ urlpatterns = [
     path('', include('women.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
