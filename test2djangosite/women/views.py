1e5bd0f4d1600cad190de554a12e5b6c05616c63
xxz <xxz911@rambler.ru>
2022-10-04 19:56:02 +0300

    Download css.styles and html pages in gitrepositories autor's youtube corse, included static file's and used filter templates

--- test2djangosite/women/views.py
+++ test2djangosite/women/views.py
@@ -1,28 +1,29 @@
-from turtle import home
-from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpResponseNotFound
-from .models import *
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
+from .models import *
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def index(request):
-    post = Women.objects.all()
-    return render(request, 'women/index.html', {'post':post, 'menu': menu, 'title': 'Главная страница'})
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu,'title': 'О сайте'})
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def categories(request, catid):
     if request.POST:
         print(request.POST)
-    return HttpResponse(f"<h1>Статьи по котегориям</h1><p>{catid}</p>")
+
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
     if int(year) > 2020:
-        return redirect(home, permanent=True)
+        return redirect('home', permanent=False)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def pageNotFound(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена!</h1>')
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
