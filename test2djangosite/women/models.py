541a4c09b4f241f50b22c52a156e4d8acc63fb0c
xxz <xxz911@rambler.ru>
2022-10-06 00:08:52 +0300

    Created new model and used ForeignKey

--- test2djangosite/women/models.py
+++ test2djangosite/women/models.py
@@ -9,6 +9,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -16,3 +17,11 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
     
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_id": self.pk})
--- test2djangosite/women/views.py
+++ test2djangosite/women/views.py
@@ -11,10 +11,14 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu, 
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0, 
         }
 
     return render(request, 'women/index.html', context=context)
@@ -36,3 +40,20 @@ def pageNotFound(request, exception):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu, 
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id, 
+        }
+
+    return render(request, 'women/index.html', context=context)
