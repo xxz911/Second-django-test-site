b0d57719e10eed74952d91958d274ffc4aeaa47f
xxz <xxz911@rambler.ru>
2022-10-08 22:19:32 +0300

    Added slug for URL's

--- test2djangosite/women/views.py
+++ test2djangosite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import *
 
@@ -36,11 +36,21 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    
+    context = {
+        'post': post,
+        'menu': menu, 
+        'title': post.title,
+        'cat_selected': post.cat_id, 
+        }
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+    return render(request, 'women/post.html', context=context)
+
+def show_category(request, cat_slug):
+    cat = Category.objects.filter(slug = cat_slug)
+    posts = Women.objects.filter(cat_id = cat[0].id)
   
 
     if len(posts) == 0:
@@ -50,7 +60,7 @@ def show_category(request, cat_id):
         'posts': posts,
         'menu': menu, 
         'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id, 
+        'cat_selected': cat[0].id 
         }
 
     return render(request, 'women/index.html', context=context)
