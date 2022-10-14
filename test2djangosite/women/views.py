6d68e2a8bd7e9e95ca2a70d5056ad9b7470e58e8
xxz <xxz911@rambler.ru>
2022-10-14 19:12:05 +0300

    Added Paginator

--- test2djangosite/women/views.py
+++ test2djangosite/women/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .forms import *
 from .models import *
